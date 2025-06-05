e127854c165ba43a181ea0fe601b36a74ccad5dd
rougenn <deziq.hi@gmail.com>
2025-06-05 11:13:42 +0300

    installer.sh fix: added ros repo key initializing

--- installer.sh
+++ installer.sh
@@ -4,6 +4,12 @@ sudo apt update
 
 sudo apt install -y python-pip git python3-jinja2
 
+# add a new ros repo key
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+
 sudo apt install -y \
   libboost-dev \
   libgnutls28-dev openssl libtiff-dev pybind11-dev \
