f774c9963b52857b36284f0580c4df4de2b395c6
rougenn <deziq.hi@gmail.com>
2025-06-05 11:58:34 +0300

    lib added

--- installer.sh
+++ installer.sh
@@ -9,6 +9,9 @@ sudo apt update && sudo apt install curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+sudo apt install -y ros-$ROS_DISTRO-camera-ros
 
 sudo apt install -y \
   libboost-dev \
